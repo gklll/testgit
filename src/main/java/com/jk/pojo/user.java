f116b10bcffda4e8a8cfc07dba16dc459129acd1
gklll <755541384@qq.com>
2019-07-03 20:39:51 +0800

    第二次提交

---
+++ src/main/java/com/jk/pojo/user.java
@@ -0,0 +1,22 @@
+package com.jk.pojo;
+
+public class user {
+    private Integer uid;
+    private String uname;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+}
